9deb9ed7f36456c9b8c84d2deeaccae118250627
Michael Gilchrist <michaelgilch@gmail.com>
2024-11-18 04:58:36 -0500

    Move quotes from embedded to json file

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 import random
 
 app = Flask(__name__)
 
-QUOTES = [
-	"Life is what happens when you're busy making other plans. - John Lennon",
-	"The best way to predict the future is to invent it. - Alan Kay",
-	"Whether you think you can or you think you can't, you're right. - Henry Ford"
-]
+with open('quotes.json', 'r') as quotes:
+	QUOTES = json.load(quotes)
 
 @app.route('/')
 def hello_world():
-	return random.choice(QUOTES)
+	selected = random.choice(QUOTES)
+	return jsonify(selected)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
