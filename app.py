5b93a5ab93cf9930d89b19a4d63b10e5d30d64df
Michael Gilchrist <michaelgilch@gmail.com>
2024-11-18 04:52:08 -0500

    Change "Hello, World" to random quote

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+QUOTES = [
+	"Life is what happens when you're busy making other plans. - John Lennon",
+	"The best way to predict the future is to invent it. - Alan Kay",
+	"Whether you think you can or you think you can't, you're right. - Henry Ford"
+]
+
 @app.route('/')
 def hello_world():
-	return "Hello, World!"
+	return random.choice(QUOTES)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
